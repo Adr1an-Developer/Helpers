3284146e9c23615b9d9a994f3638ae20782ef69d
Adrian Gonzalez <54685290+Adr1anDev@users.noreply.github.com>
2025-03-26 23:22:06 -0300

    Update adb-wifi.ps1

--- Utils-Mobile/adb-wifi.ps1
+++ Utils-Mobile/adb-wifi.ps1
@@ -1,6 +1,27 @@
-# Solicitar dirección IP y puertos al usuario
+# Para Ejecutar
+# powershell -ExecutionPolicy Bypass -File "./adb-wifi.ps1"
+#
+# Definir los archivos donde se almacenarán los puertos
+$PortFilePath = "ultimo_puerto_conexion.txt"
+$PairPortFilePath = "ultimo_puerto_emparejamiento.txt"
+
+# Leer el último puerto de conexión almacenado si existe
+if (Test-Path $PortFilePath) {
+    $PortConnect = Get-Content $PortFilePath
+} else {
+    $PortConnect = 40621 # Valor por defecto
+}
+
+# Leer el último puerto para emparejar almacenado si existe
+if (Test-Path $PairPortFilePath) {
+    $PortPair = Get-Content $PairPortFilePath
+} else {
+    $PortPair = 40063 # Valor por defecto
+}
+
+# Solicitar dirección IP y puerto al usuario
 $IpAddress = Read-Host -Prompt "Introduce la dirección IP (por defecto: 192.168.1.5)"
-$PortConnect = Read-Host -Prompt "Introduce el puerto para conectar (por defecto: 40621)"
+$PortConnect = Read-Host -Prompt "Introduce el puerto para conectar (por defecto: $PortConnect)"
 
 # Asignar valores por defecto si no se introducen
 if (-not $IpAddress) {
@@ -17,8 +38,8 @@ Write-Host "2: No emparejar, solo conectar (connect)"
 $option = Read-Host -Prompt "Ingrese el número de la opción deseada (1 o 2)"
 
 if ($option -eq "1") {
-    # Solicitar puerto para emparejar
-    $PortPair = Read-Host -Prompt "Introduce el puerto para emparejar (por defecto: 40063)"
+    # Leer el último puerto para emparejar (opcional)
+    $PortPair = Read-Host -Prompt "Introduce el puerto para emparejar (por defecto: $PortPair)"
     if (-not $PortPair) {
         $PortPair = 40063
     }
@@ -27,6 +48,9 @@ if ($option -eq "1") {
     $pairCommand = "adb pair ${IpAddress}:$PortPair"
     Invoke-Expression $pairCommand
     Write-Host "Comando ejecutado: $pairCommand"
+    
+    # Guardar el puerto de emparejamiento utilizado en el archivo
+    $PortPair | Out-File -FilePath $PairPortFilePath -Force
 }
 elseif ($option -eq "2") {
     Write-Host "No se realizará emparejamiento."
@@ -40,6 +64,9 @@ else {
 $connectCommand = "adb connect ${IpAddress}:$PortConnect"
 Invoke-Expression $connectCommand
 Write-Host "Comando ejecutado: $connectCommand"
+
+# Guardar el puerto de conexión utilizado en el archivo
+$PortConnect | Out-File -FilePath $PortFilePath -Force
 
 write-Host "*************************************************"
 write-Host "Lista de Dispositivos"
